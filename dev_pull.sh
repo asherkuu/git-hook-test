ccac443ef0011f2b47f4ce271f4b962b853641b0
bss-skkim <skkim@bss.co.kr>
2022-10-28 12:24:20 +0900

    #27

---
+++ dev_pull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# cd /var/www/bet-web-front
+
+# exec git pull origin develop
+
+# rm -rf ./node_modules
+# npm install
+
+
+# pm2 reload ecosystem.config.js
+
+echo started dev_pull.sh
