abdbd95db37bf17094a66cc79a1d0d28f6b37e09
bss-skkim <skkim@bss.co.kr>
2022-10-31 14:23:13 +0900

    no message

--- dev_pull.sh
+++ dev_pull.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# cd /var/www/bet-web-front
-
-# exec git pull origin develop
-
-# rm -rf ./node_modules
-# npm ci
-
-
-# pm2 reload ecosystem.config.js
-
-echo started dev_pull.sh
+cd /var/www/git-hook-test
+git pull
